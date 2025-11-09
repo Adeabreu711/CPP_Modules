f23086d7975dc6648fdde84bf6e35e76a49f7771
Alexandre <alexandredeabreu711@gmail.com>
2025-11-09 23:58:08 +0100

    feat : ex04 OK

---
+++ m1/ex04/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void	replaceOccurencesInLine(std::string &line, const std::string &to_replace, const std::string &new_str)
+{
+	std::string	new_line;
+	size_t		pos;
+	size_t		found_id;
+
+	if (to_replace.empty())
+		return ;
+	pos = 0;
+	while (1)
+	{
+		found_id = line.find(to_replace, pos);
+		if (found_id == std::string::npos)
+			break ;
+		new_line.append(line, pos, found_id - pos);
+		new_line.append(new_str);
+		pos = found_id + to_replace.length();
+	}
+	new_line.append(line, pos, std::string::npos);
+	line = new_line;
+}
+
+void	replaceInFile(std::ifstream &src, std::ofstream &dst, char **argv)
+{
+	std::string	line;
+
+	while (std::getline(src, line))
+	{
+		replaceOccurencesInLine(line, argv[2], argv[3]);
+		dst << line;
+		if (!src.eof())
+			dst << "\n";
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	std::string		new_filename;
+
+	if (argc != 4)
+		return (1);
+
+	new_filename = ((std::string) argv[1]).append(".replace");
+	std::ifstream src_file(argv[1]);
+	std::ofstream dst_file(new_filename.c_str());
+
+	if (!src_file.is_open() || !dst_file.is_open())
+	{
+		std::cout << "Error : file cannot be opened" << std::endl;
+		return (1);
+	}
+
+	replaceInFile(src_file, dst_file, argv);
+	return (0);
+}
+
