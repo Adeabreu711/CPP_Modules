79c085c918e082c11d9a3ed3bb9d30474922cb03
Alexandre De Abreu <alde-abr@f1br10s4.clusters.42paris.fr>
2025-11-10 13:31:30 +0100

    feat : ex05 OK

---
+++ m1/ex05/Harl.hpp
@@ -0,0 +1,20 @@
+#ifndef HARL
+# define HARL
+
+#include <iostream>
+
+class Harl
+{
+	public :
+		Harl(void);
+		~Harl(void);
+
+		void	complain(std::string level);
+	private :
+		void	debug(void);
+		void	warning(void);
+		void	info(void);
+		void	error(void);
+};
+
+#endif
