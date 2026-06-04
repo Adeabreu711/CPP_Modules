a2d2330d6fb170bdb393297f1e13de1049259838
Alexandre <alexandredeabreu711@gmail.com>
2026-06-04 17:25:18 +0200

    feat: added some test on main

--- m5/ex00/main.cpp
+++ m5/ex00/main.cpp
@@ -3,16 +3,39 @@
 
 int	main(void)
 {
-	Bureaucrat *test = new Bureaucrat("Bob", 1);
+	Bureaucrat *low = new Bureaucrat("Bob", 150);
+	Bureaucrat *medium = new Bureaucrat("Vlad", 75);
+	Bureaucrat *high = new Bureaucrat("Djimi", 1);
 
+	//HIGH_TEST
 	try 
 	{
-		test->IncrementGrade();
+		high->IncrementGrade();
 	}
 	catch(std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << e.what() << ". Info : " << *high << std::endl;
 	}
-	std::cout << *test << std::endl;
+
+	//MEDIUM_TEST
+	try 
+	{
+		medium->IncrementGrade();
+		medium->DecrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << "Info : " << *medium << std::endl;
+	}
+
+	//LOW_TEST
+	try 
+	{
+		low->DecrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << "Info : " << *low << std::endl;
+	}
 	return (0);
 }
